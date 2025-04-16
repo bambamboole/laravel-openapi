b039876762c38a49301584d991727637ad3a95bb
bambamboole <manuel.christlieb@xentral.com>
2025-04-16 16:47:03 +0200

    Add DeleteEndpoint

---
+++ src/Attributes/DeleteEndpoint.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Bambamboole\LaravelOpenApi\Attributes;
+
+use Bambamboole\LaravelOpenApi\AttributeFactory;
+use OpenApi\Annotations\Delete;
+use OpenApi\Attributes\Response;
+use OpenApi\Generator;
+
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD)]
+class DeleteEndpoint extends Delete
+{
+    public function __construct(
+        string $path,
+        ?string $description = null,
+        ?array $tags = null,
+        ?array $security = null,
+        ?string $summary = null,
+        ?array $parameters = [],
+        ?string $operationId = null,
+        array $validates = []
+    ) {
+        $responses = [
+            new Response(response: '204', description: 'Resource successfully deleted'),
+            new Response(response: '401', description: 'Unauthorized'),
+            new Response(response: '403', description: 'Unauthorized'),
+        ];
+        if (! empty($validates)) {
+            $responses[] = AttributeFactory::createValidationResponse($validates);
+        }
+
+        $parameters = array_merge($parameters, AttributeFactory::createMissingPathParameters($path, $parameters));
+
+        parent::__construct([
+            'path' => $path,
+            'operationId' => $operationId ?? Generator::UNDEFINED,
+            'description' => $description ?? Generator::UNDEFINED,
+            'summary' => $summary ?? Generator::UNDEFINED,
+            'security' => $security ?? Generator::UNDEFINED,
+            'servers' => $servers ?? Generator::UNDEFINED,
+            'tags' => $tags ?? Generator::UNDEFINED,
+            'callbacks' => $callbacks ?? Generator::UNDEFINED,
+            'deprecated' => $deprecated ?? Generator::UNDEFINED,
+            'x' => $x ?? Generator::UNDEFINED,
+            'value' => $this->combine($responses, $parameters),
+        ]);
+    }
+}
