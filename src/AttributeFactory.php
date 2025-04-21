cdd7df7f7ccbdc8b9d04cb56de89f9b5b897fec0
bambamboole <manuel.christlieb@xentral.com>
2025-04-21 11:55:19 +0200

    filter only for string rules

--- src/AttributeFactory.php
+++ src/AttributeFactory.php
@@ -155,6 +155,7 @@ class AttributeFactory
     {
         // Convert array rules to string for parsing
         if (is_array($rule)) {
+            $rule = array_filter($rule, fn($v) => is_string($v));
             $rule = implode('|', array_map(function ($item) {
                 return is_array($item) ? implode(':', $item) : $item;
             }, $rule));
