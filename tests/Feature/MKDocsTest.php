c8f28f235f78f55aeaa086f2b9ad1c3143534c45
bambamboole <manuel.christlieb@xentral.com>
2025-08-12 12:31:25 +0200

    change back MKDocsTest.php

--- tests/Feature/MKDocsTest.php
+++ tests/Feature/MKDocsTest.php
@@ -1,18 +1,16 @@
 <?php declare(strict_types=1);
 
 it('generate config and docs', function () {
-    $this->withoutExceptionHandling();
     $fs = new Illuminate\Filesystem\Filesystem;
     $expectedFolder = dirname(__DIR__, 2).'/mkdocs';
     $actualFolder = fixture('mkdocs/actual');
     $fs->deleteDirectory($actualFolder);
     $path = $actualFolder;
 
-    $returnCode = $this->withoutMockingConsoleOutput()->artisan('mkdocs:generate', ['--path' => $path]);
+    $this->artisan('mkdocs:generate', ['--path' => $path])->assertExitCode(0);
 
     expect($fs->get($actualFolder.'/mkdocs.yml'))->toEqual($fs->get($expectedFolder.'/mkdocs.yml'));
 
-    expect($returnCode)->toBe(0);
     //    $expectedFiles = $fs->allFiles($expectedFolder);
     //    foreach ($expectedFiles as $file) {
     //        $actualFile = str_replace($expectedFolder, $actualFolder, $file->getPathname());
